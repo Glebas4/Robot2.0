bf641a2136cbb7236f19c5c07fe2057a86b23e77
Glebas4 <gleb.gurov.jr@gmail.com>
2026-04-06 18:40:00 +0300

    Update obstacle_detect.py

--- Программное_Обеспечение/Примеры_Кода/obstacle_detect.py
+++ Программное_Обеспечение/Примеры_Кода/obstacle_detect.py
@@ -3,15 +3,25 @@ from robot import Robot
 
 rob = Robot()
 
-while True:
-    time.sleep(0.1)
-    dist_arr = rob.get_dist()
-    rob.set_motor(rob.R, rob.FWD, 220)
-    rob.set_motor(rob.L, rob.FWD, 130)
-
-
-    while not all(dist > 15 for dist in dist_arr):
-        rob.set_motor(rob.R, rob.BWD, 200)
-        rob.set_motor(rob.L, rob.FWD, 200)
+def main():
+    while True:
         dist_arr = rob.get_dist()
+        left, fwd, right = dist_arr
 
+        if all(dist > 15 for dist in dist_arr):
+            rob.set_motor(rob.R, rob.FWD, 255)
+            rob.set_motor(rob.L, rob.FWD, 255)
+
+        elif left < right:
+            rob.set_motor(rob.R, rob.BWD, 180)
+            rob.set_motor(rob.L, rob.FWD, 180)
+
+        else:
+            rob.set_motor(rob.R, rob.FWD, 180)
+            rob.set_motor(rob.L, rob.BWD, 180)
+
+        time.sleep(0.05)
+
+
+if __name__ == '__main__':
+    main()
